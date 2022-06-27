3bb7b211e158dfacc83f7bb0d09731321dc5740e
wangzhongjie <uvdream@163.com>
2022-06-27 14:49:20 +0800

    feat: 编辑器

--- src/pages/ediotr/editor.tsx
+++ src/pages/ediotr/editor.tsx
@@ -1,8 +1,11 @@
+import {useRef} from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import {markdown, markdownLanguage} from '@codemirror/lang-markdown';
+import {css} from '@codemirror/lang-css';
 import {languages} from '@codemirror/language-data';
 import {ViewPlugin} from "@codemirror/view";
 import {emitter, EventType} from "@/utils";
+import * as events from '@uiw/codemirror-extensions-events';
 
 type Props = {
     value?: string;
@@ -10,6 +13,16 @@ type Props = {
     onChange?: (value: string) => void;
 };
 export default function Editor(props: Props) {
+    const editor = useRef(null)
+    // 编辑器语言
+    const language = (): any => {
+        if (props.language === 'markdown') {
+            return markdown({base: markdownLanguage, codeLanguages: languages});
+        } else if (props.language === 'css') {
+            return css();
+        }
+    }
+
     const scroll = ViewPlugin.fromClass(
         class {
             constructor(view: any) {
@@ -21,13 +34,41 @@ export default function Editor(props: Props) {
         }
     );
 
+    const eventExt = events.scroll({
+        scroll: (evn) => {
+            console.log("滚动", evn);
+            // setScrollTop(evn.target.scrollTop);
+        },
+    });
+
+    const eventExt2 = events.content({
+        focus: (evn) => {
+            console.log('focus');
+            console.log(evn)
+            console.log(editor)
+        },
+        blur: (evn) => {
+            console.log('blur');
+            console.log(evn)
+        },
+    });
+    const getInstance = (instance: any) => {
+        if (instance?.state) {
+            instance.state.replaceSelection("!!!!")
+            console.log("111")
+            console.log(instance.state)
+        }
+    }
+
     return (
         <div className={"editor"}>
             <CodeMirror
                 placeholder={"请输入文章内容"}
+                width="100%"
                 height="100vh"
+                ref={getInstance}
                 value={props.value}
-                extensions={[scroll, markdown({base: markdownLanguage, codeLanguages: languages})]}
+                extensions={[scroll, eventExt, eventExt2, language()]}
             />
         </div>
     )
