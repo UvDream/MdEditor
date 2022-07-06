4c7527a8fca19bc030eac00040ff740f49404cee
wangzhongjie <uvdream@163.com>
2022-07-06 18:48:07 +0800

    feat: 列表子项样式功能

---
+++ src/pages/home/components/left-bar/article-item.tsx
@@ -0,0 +1,53 @@
+import {Button, Divider, Dropdown, Menu, Popover} from "@arco-design/web-react";
+import {FolderSuccess, SettingConfig} from "@icon-park/react";
+import "./index.less"
+
+export default function ArticleItem() {
+    const dropList = (
+        <Menu style={{width: 120}}>
+            <Menu.Item key='1'>
+                <Button type='text' style={{fontSize: 12, lineHeight: "30px", height: 30}}>
+                    发布文章
+                </Button>
+            </Menu.Item>
+            <Menu.Item key='2'>
+                <Button type='text' style={{fontSize: 12, lineHeight: "30px", height: 30}}>
+                    设置
+                </Button>
+            </Menu.Item>
+            <Divider style={{margin:"2px 0"}}/>
+            <Menu.Item key='3'>
+                <Button type='text' status='danger' style={{fontSize: 12, lineHeight: "20px", height: 20}}>
+                    删除
+                </Button>
+            </Menu.Item>
+        </Menu>
+    )
+    return (
+        <div className={"article-item"}>
+            <div className={"article-item-left"}>
+                <div className={"article-item-left-top"}>
+                    <div style={{marginTop: 2, marginRight: 2}}>
+                        <FolderSuccess theme="outline" size="15" fill="#333" strokeWidth={3}/>
+                        {/*<FolderSuccessOne theme="outline" size="24" fill="#333" strokeWidth={3}/>*/}
+                    </div>
+                    <div className={"one-line title"}>
+                        标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题
+                    </div>
+                </div>
+                <div className={"article-item-left-bottom"}>
+                    2022-02-02 11:11:11
+                </div>
+            </div>
+            <div className={"article-item-right"}>
+                <Popover
+                    // popupVisible={true}
+                    content={dropList}
+                    position={'rt'}
+                >
+                    <SettingConfig theme="outline" size="20" fill="#333" strokeWidth={3}/>
+                </Popover>
+            </div>
+        </div>
+    )
+}
--- src/pages/home/components/left-bar/article-list.tsx
+++ src/pages/home/components/left-bar/article-list.tsx
@@ -1,9 +1,11 @@
 import {Empty, Pagination} from "@arco-design/web-react";
+import ArticleItem from "@/pages/home/components/left-bar/article-item";
 
 export default function ArticleList() {
     return (
         <div className={"article-list"}>
             <Empty/>
+            <ArticleItem/>
             <div className={"article-list-pagination"}>
                 <Pagination simple total={50} size="small" className={"article-list-pagination"} />
             </div>
