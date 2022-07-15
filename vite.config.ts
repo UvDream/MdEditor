90f280dc6e07fea6b81e1e054e5d061170268f40
wangzhongjie <uvdream@163.com>
2022-07-15 18:09:59 +0800

    feat: 打包客户端

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,24 @@ import vitePluginForArco from "@arco-plugins/vite-react";
 import * as path from "path";
 export default defineConfig({
   base: "/",
+  clearScreen: false,
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
   resolve:{
     alias:{
       "@":path.resolve(__dirname,"./src")
     }
+  },
+  build: {
+    // Tauri supports es2021
+    target: ['es2021', 'chrome97', 'safari13'],
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
   plugins: [react(), vitePluginForArco({})],
 });
