5fcf9935fa6755f334de62063399092f421c86c8
wangzhongjie <uvdream@163.com>
2022-08-15 15:24:21 +0800

    feat: 打包工具

---
+++ bin/version.js
@@ -0,0 +1,50 @@
+const inquirer = require('inquirer')
+const pkg = require('../package.json')
+const tauriPkg = require('../src-tauri/tauri.conf.json')
+const fs = require("fs");
+module.exports.make_version = async () => {
+    const old = pkg.version;
+    const oldArr = old.split(".");
+    //小版本
+    const small = oldArr[0] + "." + oldArr[1] + "." + (parseInt(oldArr[2]) + 1);
+    const middle = oldArr[0] + "." + (parseInt(oldArr[1]) + 1) + "." + oldArr[2];
+    const big = parseInt(oldArr[0]) + 1 + "." + oldArr[1] + "." + oldArr[2];
+    const versions = [small, middle, big]
+    const opt = ["小版本(" + small + ")", "中版本(" + middle + ")", "大版本(" + big + ")", "其它"];
+    const question = [
+        {
+            type: "rawlist" /* 选择框 */,
+            message: "请选择发布的版本？",
+            name: "operation",
+            choices: opt,
+        }
+    ];
+    const answer = await inquirer.prompt(question);
+    opt.forEach((element, index) => {
+        if (element === answer.operation) {
+            if (index === 3) {
+                inquirer.prompt([{
+                    type: "input",
+                    message: "请输入版本号：",
+                    name: "version",
+                }]).then(v => {
+                    deploy_version(v.version)
+                })
+            } else {
+                deploy_version(versions[index])
+            }
+        }
+    })
+}
+
+function deploy_version(version) {
+    try {
+        pkg.version = version;
+        tauriPkg.package.version = version
+        fs.writeFileSync("./package.json", JSON.stringify(pkg, null, 4));
+        fs.writeFileSync("./src-tauri/tauri.conf.json", JSON.stringify(tauriPkg, null, 4));
+        //    修改完文件,开始打tag推送
+    } catch (e) {
+        console.log("错误", e)
+    }
+}
